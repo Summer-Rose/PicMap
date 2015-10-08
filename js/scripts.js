e98231e50ad2e83fc8b55f57cadc572a08e84cf3
Summer <sbrochtrup@gmail.com>
2015-10-08 15:48:01 -0700

    Add calculate distance function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var map;
 var markersArray = [];
+var myLatLng;
+var coordinates = [];
 
 function initialize() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -7,18 +9,18 @@ function initialize() {
     center: {lat: 37.09024, lng: -95.712891 }
   });
   randomLatLng();
-  ///GET PHOTOS HERE
+
   map.addListener("click", function (event) {
     deletePreviousMarker();
     var latitude = event.latLng.lat();
     var longitude = event.latLng.lng();
-    var myLatLng = {lat: latitude, lng: longitude};
+    myLatLng = {lat: latitude, lng: longitude};
     var marker = new google.maps.Marker({
       position: myLatLng,
       map: map,
     });
     markersArray.push(marker);
-    console.log( latitude + ', ' + longitude );
+    console.log("MarkersArray =" + markersArray);
   });
 }
 
@@ -40,7 +42,7 @@ function randomLatLng() {
   };
   var lngSpan = bounds.east - bounds.west;
   var latSpan = bounds.north - bounds.south;
-  var coordinates = [];
+  //var coordinates = [];
   var lat = bounds.south + latSpan * Math.random();
   var lng = bounds.west + lngSpan * Math.random();
   coordinates.push(lat);
@@ -49,8 +51,30 @@ function randomLatLng() {
   return coordinates;
 }
 
+function calculateDifference() { //can later add unit as parameter
+  if (markersArray.length > 0) {
+    var R = 6371;
+    var latGuess = markersArray[0].position.J;
+    var lngGuess = markersArray[0].position.M;
+    var latActual = coordinates[0];
+    var lngActual = coordinates[1];
+    var dLat = deg2rad(latActual-latGuess);
+    var dLon = deg2rad(lngActual-lngGuess);
+    var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(latGuess)) * Math.cos(deg2rad(latActual)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var distance = R * c; // Distance in km
+    return distance;
 
+  }
+}
 
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
 // map.addListener('click', function() {
 //   infowindow.open(marker.get('map'), marker);
