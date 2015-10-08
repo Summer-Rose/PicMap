cb8d133c97a7a689e7f4d4fa02ca54b04e0afd84
Summer <sbrochtrup@gmail.com>
2015-10-08 11:55:38 -0700

    fix merge conflict

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-// var functionName = function(params) {
-//
-// };
+function initialize() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: 37.09024, lng: -95.712891 }
+  });
+  randomLatLng();
+}
 
-// $(document).ready(function() {
-//
-// });
+function randomLatLng() {
+  var bounds = {
+    north: 50.2116931,
+    south: 25.9701932,
+    east: -66.9732228,
+    west: -123.9639528
+  };
+
+  var lngSpan = bounds.east - bounds.west;
+  var latSpan = bounds.north - bounds.south;
+  var coordinates = [];
+  var lat = bounds.south + latSpan * Math.random();
+  var lng = bounds.west + lngSpan * Math.random();
+  coordinates.push(lat);
+  coordinates.push(lng);
+  console.log(coordinates);
+  return coordinates;
+}
+
+
+
+
+map.addListener('click', function() {
+  infowindow.open(marker.get('map'), marker);
+})
+
+// // $(document).ready(function() {
+// //
+// // });
