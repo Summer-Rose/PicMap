4f060feb4268a6881ac1c06630cc3cf63a375b57
Summer <sbrochtrup@gmail.com>
2015-10-08 13:50:40 -0700

    Add function to display single marker on map click

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
+var map;
+var markersArray = [];
+
 function initialize() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: {lat: 37.09024, lng: -95.712891 }
   });
   randomLatLng();
+  ///GET PHOTOS HERE
+  map.addListener("click", function (event) {
+    deletePreviousMarker();
+    var latitude = event.latLng.lat();
+    var longitude = event.latLng.lng();
+    var myLatLng = {lat: latitude, lng: longitude};
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+    });
+    markersArray.push(marker);
+    console.log( latitude + ', ' + longitude );
+  });
+}
+
+function deletePreviousMarker() {
+  if (markersArray) {
+  for (i in markersArray) {
+    markersArray[i].setMap(null);
+  }
+  markersArray.length = 0;
+  }
 }
 
 function randomLatLng() {
@@ -13,7 +38,6 @@ function randomLatLng() {
     east: -66.9732228,
     west: -123.9639528
   };
-
   var lngSpan = bounds.east - bounds.west;
   var latSpan = bounds.north - bounds.south;
   var coordinates = [];
@@ -28,9 +52,9 @@ function randomLatLng() {
 
 
 
-map.addListener('click', function() {
-  infowindow.open(marker.get('map'), marker);
-})
+// map.addListener('click', function() {
+//   infowindow.open(marker.get('map'), marker);
+// })
 
 // // $(document).ready(function() {
 // //
